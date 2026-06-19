8cbf5b21737d1f3d7365e04a51793c91769989be
Gouthaman Gnanamani <gpg210302@gmail.com>
2026-06-19 15:57:47 +0200

    update address

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -12,7 +12,7 @@ export const SITE = {
   phone: "+48 573 033 220",
   email: "admin@la-neuron.org",
   location: "Kraków",
-  venue: "Kraków, Poland",
+  venue: "ul. Śniadeckich 3,Kraków, Poland",
   social: { facebook: "https://www.facebook.com/share/1B87gctcBG/?mibextid=wwXIfr", instagram: "https://www.instagram.com/laneuron?igsh=MWxpOXRkdjh3bzBpbw==", youtube: "#" }, // placeholders – to be added later
 };
 
